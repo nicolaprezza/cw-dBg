41a34694e6523b392313f6c4d9aa1f602157d7e2
nicolaprezza <nicolapr@gmail.com>
2020-12-05 12:18:20 +0100

    it works!

--- cw-dBg-check.cpp
+++ cw-dBg-check.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <chrono>
 #include "internal/cw-dBg.hpp"
+#include <unordered_map>
 
 //using namespace dBg;
 using namespace std;
@@ -10,17 +11,19 @@ using namespace dbg;
 format_t format = fastq;
 
 uint64_t n = 1000;
-int prob = 1;
+int prob = 100;
 
+uint64_t nlines = 0;
 
 void help(){
-	cout << "cw-dBg-benchmark: benchmarks a previously constructed weighted de Bruijn graph." << endl << endl;
-	cout << "Usage: cw-dBg-benchmark [options] <input_index> <input_fastx>" << endl;
+	cout << "cw-dBg-check: checks a previously constructed weighted de Bruijn graph." << endl << endl;
+	cout << "Usage: cw-dBg-check [options] <input_index> <input_fastx>" << endl;
 	cout << "   Options:"<<endl;
+	cout << "   -l <nlines>         Use only the first nlines sequences from the input file to build the graph. If set to 0, use all lines. Default: 0."<<endl;
 	cout << "   -n                  Number of k-mers to test. Default: 1000"<<endl;
 	cout << "   -a                  The input file is fasta. If not specified, it is assumed that the input file is fastq."<<endl;
 	cout << "   <input_index>       Input index built with cw-dbg-build. Mandatory."<<endl;
-	cout << "   <input_fastx>       Fasta/fastq file from which test kmers will be extracted."<<endl;
+	cout << "   <input_fastx>       Fasta/fastq file from which test kmers will be extracted. Must be the same on which the index was built."<<endl;
 	exit(0);
 }
 
@@ -43,6 +46,15 @@ void parse_args(char** argv, int argc, int &ptr){
 	}else if(s.compare("-a")==0){
 
 		format = fasta;
+
+	}else if(s.compare("-l")==0){
+
+		if(ptr>=argc-2){
+			cout << "Error: missing parameter after -l option." << endl;
+			help();
+		}
+
+		nlines = atoi(argv[ptr++]);
 
 	}else{
 		cout << "Error: unrecognized '" << s << "' option." << endl;
@@ -88,6 +100,59 @@ int main(int argc, char** argv){
 	auto k = cwdbg.get_order();
 
 
+	/*
+	 * builds a simple hash table with kmer counts
+	 * note: we convert any non-DNA character to A (as it is done in the index)
+	 */
+
+	cout << "Building counts with a hash table ... " << endl;
+
+	unordered_map<string, uint64_t> counts;
+
+	{
+
+		ifstream file(input_fastx);
+
+		string str;
+		uint64_t n_seq = 0;
+
+		while (std::getline(file, str) and n_seq < nlines) { //getline reads header
+
+			getline(file, str);//getline reads DNA
+
+			for(char & c:str)
+				if(c!='A' and c!='C' and c!='G' and c!='T') c = 'A';
+
+			string kmer = str.substr(0,k-1);
+
+			for(int i=k-1;i<str.length();++i){
+
+				kmer += str[i];
+
+				counts[kmer] = counts[kmer]+1;
+
+				kmer = kmer.substr(1,k-1);
+
+			}
+
+
+			n_seq++;
+
+			if(n_seq%100000==0)
+				cout << "processed " << n_seq << " sequences" << endl;
+
+			if(format == fastq){
+				getline(file, str);//getline reads +
+				getline(file, str);//getline reads quality
+			}
+
+		}
+
+	}
+
+
+	cout << "Testing ... " << endl;
+
 	srand(time(NULL));
 
 	ifstream file(input_fastx);
@@ -95,7 +160,11 @@ int main(int argc, char** argv){
 	uint64_t m = 0;//extracted kmers
 
 	string str;
-	while (std::getline(file, str)) { //getline reads header
+
+	int prev_perc = 0;
+	int perc = 0;
+
+	while (std::getline(file, str) and m<=n) { //getline reads header
 
 		getline(file, str);//getline reads DNA
 
@@ -107,8 +176,23 @@ int main(int argc, char** argv){
 
 			if((rand() % 100) < prob){
 
-				cout << kmer << " : " << cwdbg[kmer] << endl;
 				m++;
+
+				if(counts[kmer] != cwdbg[kmer]){
+
+					cout << "Error: " << kmer << " : " << cwdbg[kmer] << " / " << counts[kmer] << endl;
+					exit(0);
+
+				}else{
+
+					perc = (m*100)/n;
+
+					if(perc > prev_perc + 4){
+						prev_perc = perc;
+						cout << (m*100)/n << "% done." << endl;
+					}
+
+				}
 
 			}
 
